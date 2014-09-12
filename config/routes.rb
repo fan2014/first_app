9f6b57b7cad4f6eaf16940aae66c14858b434b45
Fan <FZHENG1@madisoncollege.edu>
2014-09-12 01:14:36 +0000

    ready to be graded

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ FirstApp::Application.routes.draw do
   get "static_pages/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  
+  # map.connect '/help/new.aspx' => 'welcome#help'
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
